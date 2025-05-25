2ad83f2d11bb2e4a52c2cac455a4cf17febbcacf
lethalgem <iwc123@gmail.com>
2025-05-24 20:45:06 -0400

    Complete M5L2

--- M05.turn_on_lights_workbook/player/player_fps_controller.gd
+++ M05.turn_on_lights_workbook/player/player_fps_controller.gd
@@ -1,5 +1,10 @@
 class_name PlayerFPSController extends CharacterBody3D
 
+@export_range(0.001, 1.0) var mouse_sensitivity := 0.005
+@export_range(0.001, 1.0) var joystick_sensitivity := 0.005
+
+@onready var _camera: Camera3D = %Camera3D
+
 func _unhandled_input(event: InputEvent) -> void:
 	var is_mouse_button := event is InputEventMouseButton
 	var is_mouse_captured := Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
@@ -9,3 +14,22 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif is_escape_pressed and is_mouse_captured:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if (event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
+		var look_offset_2d: Vector2 = event.screen_relative * mouse_sensitivity
+		_rotate_camera_by(look_offset_2d)
+
+func _process(delta: float) -> void:
+	var look_vector := Input.get_vector("look_left", "look_right", "look_up", "look_down")
+	var look_offset_2d: Vector2 = look_vector * joystick_sensitivity * delta
+	_rotate_camera_by(look_offset_2d)
+
+func _rotate_camera_by(look_offset_2d: Vector2) -> void:
+	_camera.rotation.y -= look_offset_2d.x
+	_camera.rotation.x -= look_offset_2d.y
+	_camera.rotation.y = wrapf(_camera.rotation.y, -PI, PI)
+	
+	const MAX_VERTICAL_ANGLE := PI / 3.0
+	_camera.rotation.x = clampf(_camera.rotation.x, -1.0 * MAX_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE)
+	
+	_camera.orthonormalize()
