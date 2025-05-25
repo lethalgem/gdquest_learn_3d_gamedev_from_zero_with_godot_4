5ffbc0271193635dffe3fecb09cf95237a846c30
lethalgem <iwc123@gmail.com>
2025-05-24 20:54:14 -0400

    Give joystick and mouse deadzones and track which was the last used input source

--- M05.turn_on_lights_workbook/player/player_fps_controller.gd
+++ M05.turn_on_lights_workbook/player/player_fps_controller.gd
@@ -5,6 +5,8 @@ class_name PlayerFPSController extends CharacterBody3D
 
 @onready var _camera: Camera3D = %Camera3D
 
+var is_using_gamepad := false
+
 func _unhandled_input(event: InputEvent) -> void:
 	var is_mouse_button := event is InputEventMouseButton
 	var is_mouse_captured := Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
@@ -12,17 +14,25 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if is_mouse_button and not is_mouse_captured:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		is_using_gamepad = false
 	elif is_escape_pressed and is_mouse_captured:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 	if (event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
-		var look_offset_2d: Vector2 = event.screen_relative * mouse_sensitivity
-		_rotate_camera_by(look_offset_2d)
+		var mouse_deadzone = 0.2
+		if event.screen_relative.length() > mouse_deadzone:
+			is_using_gamepad = false
+			var look_offset_2d: Vector2 = event.screen_relative * mouse_sensitivity
+			_rotate_camera_by(look_offset_2d)
 
 func _process(delta: float) -> void:
 	var look_vector := Input.get_vector("look_left", "look_right", "look_up", "look_down")
-	var look_offset_2d: Vector2 = look_vector * joystick_sensitivity * delta
-	_rotate_camera_by(look_offset_2d)
+	
+	var joystick_deadzone = 0.2
+	if look_vector.length() > joystick_deadzone:
+		is_using_gamepad = true
+		var look_offset_2d: Vector2 = look_vector * joystick_sensitivity * delta
+		_rotate_camera_by(look_offset_2d)
 
 func _rotate_camera_by(look_offset_2d: Vector2) -> void:
 	_camera.rotation.y -= look_offset_2d.x
